b3c384d7345891f5f60158b107b2ada59e89860a
agoldvarg <agoldvarg@gmail.com>
2015-02-19 20:59:14 -0500

    add basic structure

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+gem 'nokogiri'
+gem 'twitter'
+gem 't'
+gem 'rspec'
+gem 'pry'
+
---
+++ config/environment.rb
@@ -0,0 +1,6 @@
+require 'bundler/setup'
+
+Bundler.require
+
+# require_relative "../app/concerns/twitter_scraper"
+# require_relative "sql_runner"
