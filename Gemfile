c324ecaeb196267368cb409cce209776ac0f5963
rnackman <rachel.nackman@gmail.com>
2015-02-23 15:35:11 -0500

    Create structure.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,11 @@
 source "https://rubygems.org"
 
+gem "rspec"
+gem "activerecord"
+gem "sqlite3"
+gem "rake"
+gem "database_cleaner"
 gem 'nokogiri'
 gem 'twitter'
 gem 't'
-gem 'rspec'
 gem 'pry'
--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,30 @@
 require 'bundler/setup'
-require 'open-uri'
-
 Bundler.require
+
+require 'rake'
+require 'active_record'
+require 'yaml/store'
+require 'ostruct'
+require 'date'
+require 'open-uri'
 
 require_relative "../app/concerns/tweet_grabber"
 require_relative "../app/concerns/clock.rb"
 require_relative "../app/concerns/wiki_scraper.rb"
 require_relative "../app/concerns/gracehoppertime.rb"
+
+DBNAME = "set_db_name"
+
+DBRegistry[ENV["ACTIVE_RECORD_ENV"]].connect!
+DB = ActiveRecord::Base.connection
+
+if ENV["ACTIVE_RECORD_ENV"] == "test"
+  ActiveRecord::Migration.verbose = false
+end
+
+RAKE_APP ||= begin
+  app = Rake.application
+  app.init
+  app.load_rakefile
+  app
+end
